544493247b2afe2349089366bd16957703002510
Iszhangbingbing <38526528+Iszhangbingbing@users.noreply.github.com>
2018-04-21 12:13:11 +0800

    Update AccountController.java
    产生一个冲突

--- src/classes/com/cn/controller/AccountController.java
+++ src/classes/com/cn/controller/AccountController.java
@@ -6,6 +6,6 @@ public class AccountController {
 	private AccountDao dao = new AccountDao();
 	public void transfer(int fId,int tId,String money){
 		dao.transfer(fId, tId, money);
-		System.out.println("hahahha");
+		System.out.println("hahahhahehehhehe");
 	}
 }
