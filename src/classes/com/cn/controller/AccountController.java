4b3509103a543a40a17b97659f13f593f542fb7f
zbbpoplar <zbbpoplar@163.com>
2018-04-21 12:26:58 +0800

    修改AccountController，添加输出语句

--- src/classes/com/cn/controller/AccountController.java
+++ src/classes/com/cn/controller/AccountController.java
@@ -7,5 +7,6 @@ public class AccountController {
 	public void transfer(int fId,int tId,String money){
 		dao.transfer(fId, tId, money);
 		System.out.println("what fucking");
+        System.out.println("you are a fool");
 	}
 }
